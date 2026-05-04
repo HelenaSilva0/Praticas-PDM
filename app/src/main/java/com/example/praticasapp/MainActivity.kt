cdf0d143ee026e3dadc51da7a9bb4826ddadf98f
Maria Helena da Silva <m.helenadasilva167@gmail.com>
2026-05-04 15:28:56 -0300

    pratica 01 parte 3

--- app/src/main/java/com/example/praticasapp/MainActivity.kt
+++ app/src/main/java/com/example/praticasapp/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.praticasapp
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.praticasapp.ui.theme.PraticasAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +30,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             PraticasAPPTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomePage(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun HomePage(modifier: Modifier = Modifier) {
+        val activity = LocalContext.current as Activity
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PraticasAPPTheme {
-        Greeting("Android")
+        Column(
+            modifier = modifier
+                .padding(24.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Seja bem-vindo à tela principal!",
+                fontSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            Button(
+                onClick = {
+                    activity.finish()
+                }
+            ) {
+                Text("Sair")
+            }
+        }
     }
 }
