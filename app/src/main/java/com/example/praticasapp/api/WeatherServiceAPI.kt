56fb0beddafdde19a77c9a1f5675d925b64a4e89
Maria Helena <m.helenadasilva167@gmail.com>
2026-06-27 21:10:20 -0300

    pratica 07 parte 2: ciacao do servico clima e localizacao

---
+++ app/src/main/java/com/example/praticasapp/api/WeatherServiceAPI.kt
@@ -0,0 +1,14 @@
+package com.example.praticasapp.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherServiceAPI {
+    companion object {
+        const val BASE_URL = "https://api.weatherapi.com/v1/"
+    }
+    // Procura a localização baseado no nome ou coordenadas
+    @GET("search.json?lang=pt_br")
+    fun search(@Query("key") key: String, @Query("q") query: String): Call<List<APILocation>?>
+}
